09a4bedd1cf55e43701172906f40110e07da4fa6
mabrowncd <mabrowncd@MA-CTC-CBROWN>
2024-12-01 20:07:53 -0500

    Key test

--- Program.cs
+++ Program.cs
@@ -5,8 +5,9 @@
         // We'll define our block class in "Block.cs" and use it here.
         static void Main(string[] args)
         {
-            
-
+            // Create a new block and set some properties
+            // Create a few more blocks using different variables
+            // Interact with the blocks
         }
     }
 }
