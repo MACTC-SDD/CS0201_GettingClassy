927a46f3c91a7877bb099f8546edebc305afd713
mabrowncd <mabrowncd@MA-CTC-CBROWN>
2024-12-01 20:00:58 -0500

    Inital file upload w/.ignore

---
+++ Block.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS0201_GettingClassy
+{
+
+    internal class Block
+    {
+        // Properties //
+        public string Id { get; set; }
+
+        // Methods //
+        public void Interact(int playerId, string action)
+        {
+            Console.WriteLine($"Player {playerId} {action} block {Id}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CS0201_GettingClassy
+{
+    internal class Program
+    {
+        // We'll define our block class in "Block.cs" and use it here.
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
